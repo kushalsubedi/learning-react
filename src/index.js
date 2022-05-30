dd107f16bb2c78974e7dac9807e3151efeaae51c
kushal <70305822+kushal309@users.noreply.github.com>
2022-05-30 13:29:59 +0545

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(<h1 className="h1">Hello My name is Kushal</h1>);
